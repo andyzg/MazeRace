cbd05f3de8c435c72fa367bee2a3ac414dd6b07d
andyzg <zhangandy94@gmail.com>
2013-12-26 14:45:08 -0500

    Fixed bug for array index out of bounds

--- MazeRace/src/MazeGen.java
+++ MazeRace/src/MazeGen.java
@@ -1,13 +1,25 @@
+import java.util.Random;
 
 public class MazeGen {
 
-	public static final int size = 20;
+	public static final int size = 6;
+	private static Random rand = new Random();
 	
 	public static void main(String[] args)
 	{
 		Cell [][]maze = gridGen(size,size);
 		generateWalls(maze);
 
+		for (int i=0; i<size; i++)
+		{
+			for (int j=0; j<size; j++)
+			{
+				System.out.print(maze[i][j].getWalls().size());
+			}
+			System.out.println();
+		}
+		mazeGenerator(maze, 0, 0);
+		
 		for (int i=0; i<size; i++)
 		{
 			for (int j=0; j<size; j++)
@@ -60,8 +72,47 @@ public class MazeGen {
 		a.removeWall(wall.getID());
 	}
 	
-	private static void mazeGenerator(Cell[][] maze)
+	private static void mazeGenerator(Cell[][] maze, int i, int j)
 	{
+		maze[i][j].setVisited(true);
+		int wallAmount = maze[i][j].getWalls().size();
 		
+		while (hasVisited(maze,i,j,wallAmount))
+		{
+			int index = rand.nextInt(wallAmount);
+			// System.out.println(index + " is the random number and " + wallAmount + " is the size");
+		
+			Wall adjacentWall= maze[i][j].getWalls()
+					.get(index);
+			
+			System.out.println("i: " + adjacentWall.getConnectedCell(maze[i][j]).getX());
+			System.out.println("j: " + adjacentWall.getConnectedCell(maze[i][j]).getY());
+			System.out.println();
+			
+			if (!adjacentWall.getConnectedCell(maze[i][j]).getVisited())
+			{
+				removeWall(adjacentWall, maze[i][j]);
+				wallAmount--;
+				mazeGenerator(maze, 
+						adjacentWall.getConnectedCell(maze[i][j]).getX(),
+						adjacentWall.getConnectedCell(maze[i][j]).getY());
+				
+				System.out.println("Wall Amount" + wallAmount);
+			}
+			return;
+		}
+	}
+	
+	private static boolean hasVisited(Cell maze[][], int i, int j, int size) {
+		int visited = 0;
+		
+		for (Wall wall:maze[i][j].getWalls())
+		{
+			if (wall.getConnectedCell(maze[i][j]).getVisited())
+				visited++;
+		}
+		if (size <= visited)
+			return false;
+		return true;
 	}
 }
